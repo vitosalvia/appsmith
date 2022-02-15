62b67f6b3599f65ee7781b5335cbaaa3a3da2186
balajisoundar <balaji@appsmith.com>
2022-02-15 16:29:36 +0530

    i

--- app/client/src/utils/WidgetRegistry.tsx
+++ app/client/src/utils/WidgetRegistry.tsx
@@ -142,6 +142,9 @@ import VideoWidget, {
   CONFIG as VIDEO_WIDGET_CONFIG,
 } from "widgets/VideoWidget";
 import { registerWidget, WidgetConfiguration } from "./WidgetRegisterHelpers";
+import TableWidgetV2, {
+  CONFIG as TABLE_WIDGET_CONFIG_V2,
+} from "widgets/TableWidgetV2";
 
 export const ALL_WIDGETS_AND_CONFIG = [
   [CanvasWidget, CANVAS_WIDGET_CONFIG],
@@ -196,6 +199,7 @@ export const ALL_WIDGETS_AND_CONFIG = [
   [PhoneInputWidget, PHONE_INPUT_WIDGET_V2_CONFIG],
   [CurrencyInputWidget, CURRENCY_INPUT_WIDGET_V2_CONFIG],
   [JSONFormWidget, JSON_FORM_WIDGET_CONFIG],
+  [TableWidgetV2, TABLE_WIDGET_CONFIG_V2],
   /*
    * If a newly added widget works well inside the list widget,
    * please add widget type in the List widget's allowed widget
