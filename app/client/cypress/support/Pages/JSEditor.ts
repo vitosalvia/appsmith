67203ac19cda132c7a57d75fe027d2959f172beb
ohansFavour <fohanekwu@gmail.com>
2022-03-29 09:53:16 +0100

    Update cypress tests

--- app/client/cypress/support/Pages/JSEditor.ts
+++ app/client/cypress/support/Pages/JSEditor.ts
@@ -6,11 +6,11 @@ const agHelper = new AggregateHelper();
 const locator = new CommonLocators();
 
 export class JSEditor {
-  private _runButton = "//li//*[local-name() = 'svg' and @class='run-button']";
+  private _runButton = "button.run-js-action";
   private _jsObjName = ".t--js-action-name-edit-field span";
   private _jsObjTxt = ".t--js-action-name-edit-field input";
-  private _newJSobj = "span:contains('New JS Object')"
-  private _bindingsClose = ".t--entity-property-close"
+  private _newJSobj = "span:contains('New JS Object')";
+  private _bindingsClose = ".t--entity-property-close";
 
   public NavigateToJSEditor() {
     cy.get(locator._createNew)
@@ -19,7 +19,11 @@ export class JSEditor {
     cy.get(this._newJSobj).click({ force: true });
 
     //cy.waitUntil(() => cy.get(locator._toastMsg).should('not.be.visible')) // fails sometimes
-    agHelper.WaitUntilEleDisappear(locator._toastMsg, 'created successfully', 1000)
+    agHelper.WaitUntilEleDisappear(
+      locator._toastMsg,
+      "created successfully",
+      1000,
+    );
   }
 
   public CreateJSObject(JSCode: string, paste = true, completeReplace = false) {
@@ -29,15 +33,19 @@ export class JSEditor {
       cy.get(locator._codeMirrorTextArea)
         .first()
         .focus()
-        .type("{downarrow}{downarrow}{downarrow}{downarrow}  ")
-    }
-    else {
+        .type("{downarrow}{downarrow}{downarrow}{downarrow}  ");
+    } else {
       cy.get(locator._codeMirrorTextArea)
-      .first()
-      .focus()
-      .type("{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}")
-      .type("{shift}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}", { force: true })
-      .type("{backspace}",{ force: true });
+        .first()
+        .focus()
+        .type(
+          "{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}",
+        )
+        .type(
+          "{shift}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}",
+          { force: true },
+        )
+        .type("{backspace}", { force: true });
 
       // .type("{uparrow}", { force: true })
       // .type("{ctrl}{shift}{downarrow}", { force: true })
@@ -48,8 +56,7 @@ export class JSEditor {
       //   .type("{ctrl}{shift}{downarrow}", { force: true })
       //   .type("{backspace}",{ force: true });
       //.type("{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow} ")
-
-      }
+    }
 
     cy.get(locator._codeMirrorTextArea)
       .first()
@@ -57,30 +64,36 @@ export class JSEditor {
         const input = cy.get(el);
         if (paste) {
           //input.invoke("val", value);
-          agHelper.Paste(el, JSCode)
+          agHelper.Paste(el, JSCode);
         } else {
           input.type(JSCode, {
-            parseSpecialCharSequences: false, delay: 150
+            parseSpecialCharSequences: false,
+            delay: 150,
           });
         }
       });
 
-    agHelper.WaitAutoSave()//Ample wait due to open bug # 10284
-    agHelper.Sleep(5000)//Ample wait due to open bug # 10284
+    agHelper.WaitAutoSave(); //Ample wait due to open bug # 10284
+    agHelper.Sleep(5000); //Ample wait due to open bug # 10284
 
     //clicking 1 times & waits for 3 second for result to be populated!
     Cypress._.times(1, () => {
-      cy.xpath(this._runButton)
+      cy.get(this._runButton)
         .first()
         .click()
-        .wait(3000)
-    })
-    cy.get(locator._empty).should('not.exist')
-    cy.get(locator._toastMsg).should("have.length", 0)
-    this.GetJSObjectName()
+        .wait(3000);
+    });
+    cy.get(locator._empty).should("not.exist");
+    cy.get(locator._toastMsg).should("have.length", 0);
+    this.GetJSObjectName();
   }
 
-  public EnterJSContext(endp: string, value: string, paste = true, toToggleOnJS = false) {
+  public EnterJSContext(
+    endp: string,
+    value: string,
+    paste = true,
+    toToggleOnJS = false,
+  ) {
     if (toToggleOnJS) {
       cy.get(locator._jsToggle(endp))
         .invoke("attr", "class")
@@ -102,7 +115,9 @@ export class JSEditor {
     cy.focused().then(($cm: any) => {
       if ($cm.contents != "") {
         cy.log("The field is not empty");
-        cy.get(locator._propertyControl + endp + " " + locator._codeMirrorTextArea)
+        cy.get(
+          locator._propertyControl + endp + " " + locator._codeMirrorTextArea,
+        )
           .first()
           .click({ force: true })
           .focused()
@@ -110,14 +125,16 @@ export class JSEditor {
             force: true,
           });
       }
-      agHelper.Sleep()
-      cy.get(locator._propertyControl + endp + " " + locator._codeMirrorTextArea)
+      agHelper.Sleep();
+      cy.get(
+        locator._propertyControl + endp + " " + locator._codeMirrorTextArea,
+      )
         .first()
         .then((el: any) => {
           const input = cy.get(el);
           if (paste) {
             //input.invoke("val", value);
-            agHelper.Paste(el, value)
+            agHelper.Paste(el, value);
           } else {
             input.type(value, {
               parseSpecialCharSequences: false,
@@ -125,7 +142,7 @@ export class JSEditor {
           }
         });
     });
-    agHelper.WaitAutoSave()//Allowing time for Evaluate value to capture value
+    agHelper.WaitAutoSave(); //Allowing time for Evaluate value to capture value
   }
 
   public RenameJSObjFromForm(renameVal: string) {
@@ -148,13 +165,14 @@ export class JSEditor {
   }
 
   public GetJSObjectName() {
-    cy.get(this._jsObjName).invoke("text").then((text) => cy.wrap(text).as("jsObjName")
-    );
+    cy.get(this._jsObjName)
+      .invoke("text")
+      .then((text) => cy.wrap(text).as("jsObjName"));
   }
 
   public validateDefaultJSObjProperties(jsObjName: string) {
     agHelper.ActionContextMenuByEntityName(jsObjName, "Show Bindings");
-    cy.get(apiwidget.propertyList).then(function ($lis) {
+    cy.get(apiwidget.propertyList).then(function($lis) {
       expect($lis).to.have.length(4);
       expect($lis.eq(0).text()).to.be.oneOf([
         "{{" + jsObjName + ".myFun2()}}",
@@ -169,5 +187,4 @@ export class JSEditor {
     });
     cy.get(this._bindingsClose).click({ force: true });
   }
-
 }
